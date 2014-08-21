24d47bb9a73165ca405f946207a70497bd179d12
Alex Mazure <alexcmazure@gmail.com>
2014-08-20 23:50:25 -0400

    removed version from setup in attempt to fix pip bugs with elastic beanstalk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='neo4django',
-    version='0.1.8',
+    # version='0.1.8',
     author='Matt Luongo',
     author_email='mhluongo@gmail.com',
     description='A Django/Neo4j ORM layer.',
